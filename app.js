593715c19963b456c9f99da358423707b002e143
Brian Nkhata <bnkhata@mra.mw>
2023-03-24 11:52:20 +0200

    added views

--- app.js
+++ app.js
@@ -1,34 +1,48 @@
-const express = require("express");
-var mongoose = require("mongoose");
-
-const app = express();
-
-app.use(express.static(__dirname));
-
-//const conString = "mongodb://admin:admin@ds038319.mlab.com:38319/mylearning"
-const conString = "mongodb://localhost:27017/";
-app.use(express.static(__dirname));
-
-const Chats = mongoose.model("Chats", {
-  name: String,
-  chat: String,
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var bodyParser = require("body-parser");
+var flash = require("express-flash");
+var session = require("express-session");
+var usersRouter = require("./routes/users");
+var app = express();
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(logger("dev"));
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: "123456catr",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 60000,
+    },
+  })
+);
+app.use(flash());
+app.use("/", usersRouter);
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
 });
-
-mongoose.connect(conString, { useMongoClient: true }, (err) => {
-  console.log("Database connection", err);
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
-
-app.post("/chats", async (req, res) => {
-  try {
-    var chat = new Chats(req.body);
-    await chat.save();
-    res.sendStatus(200);
-  } catch (error) {
-    res.sendStatus(500);
-    console.error(error);
-  }
-});
-
-app.listen(3020, () => {
-  console.log("Well done, now I am listening...");
-});
+module.exports = app;
